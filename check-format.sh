deb342e683d20310b380464797bd2b2d112efa64
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2025-10-26 18:56:41 -0300

    Add clang-format checks

---
+++ check-format.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+failed_files=$( \
+	find src -type f \( -name "*.cpp" -o -name "*.h" \) ! -path "src/lib/fb-api/*" \
+		-exec sh -c 'for f; do [ "$(clang-format "$f")" != "$(cat "$f")" ] && echo "$f"; done' _ {} +)
+
+if [ -n "$failed_files" ]; then
+	echo "The following files are not properly formatted:"
+	echo "$failed_files"
+	exit 1
+fi
