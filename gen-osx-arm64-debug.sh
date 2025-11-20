b1217448f5376f14d576704e075a25ccc08cbf10
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2025-11-20 10:12:56 -0300

    Triplets rework

--- gen-osx-arm64-debug.sh
+++ gen-osx-arm64-debug.sh
@@ -2,6 +2,4 @@
 cmake -S . -B build/Debug \
 	-DCMAKE_BUILD_TYPE=Debug \
 	-DCMAKE_VERBOSE_MAKEFILE=ON \
-	-DVCPKG_TARGET_TRIPLET=arm64-osx-custom \
-	-DVCPKG_OVERLAY_TRIPLETS=cmake/triplets \
 	-G Ninja
