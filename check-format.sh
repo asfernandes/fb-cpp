47a71b65f7a7d7b8515526fe9ccbdfc11b7aeb5d
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2026-04-09 12:26:06 -0300

    Use clang-format-20

--- check-format.sh
+++ check-format.sh
@@ -2,7 +2,7 @@
 
 failed_files=$( \
 	find src -type f \( -name "*.cpp" -o -name "*.h" \) \
-		-exec sh -c 'for f; do [ "$(clang-format "$f")" != "$(cat "$f")" ] && echo "$f"; done' _ {} +)
+		-exec sh -c 'for f; do [ "$(clang-format-20 "$f")" != "$(cat "$f")" ] && echo "$f"; done' _ {} +)
 
 if [ -n "$failed_files" ]; then
 	echo "The following files are not properly formatted:"
